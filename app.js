2d5f203856510931d2637532bd7c19678f7fec63
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-03-25 00:54:57 +0530

    adding bootstrap and basic styling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground.js');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 const { urlencoded } = require('express');
 
@@ -20,6 +21,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views');
 app.set('views', path.join(__dirname, 'views'));
