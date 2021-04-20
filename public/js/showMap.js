ac179beb7e42ebf2a3f0295e29b80e60953718b9
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-04-21 00:48:16 +0530

    adding cluster map

--- public/js/showMap.js
+++ public/js/showMap.js
@@ -1,11 +1,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
     center: camp.geometry.coordinates, // starting position [lng, lat]
     zoom: 12 // starting zoom
 });
 
-const marker = new mapboxgl.Marker()
-    .setLngLat([camp.geometry.coordinates])
+new mapboxgl.Marker()
+    .setLngLat(camp.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offsset: 25 })
+            .setHTML(
+            `<h3>${camp.title}</h3><p>${camp.location}</p>`
+        )
+    )
     .addTo(map);
--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('../models/campground.js');
-const cities = require('./cities.js');
+const cities = require('./india.js');
 const { places, descriptors } = require('./seedHelpers.js');
 
 mongoose.connect('mongodb://localhost:27017/campgrounds', {
@@ -22,12 +22,16 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
-        const random = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
+        const random = Math.floor(Math.random() * 100);
+        const price = Math.floor(Math.random() * 200) + 10;
         const camp = new Campground({
             author: '607afdfb64144345a418704e',
-            location: `${cities[random].name}, ${cities[random].state}`,
+            location: `${cities[random].city}, ${cities[random].admin_name}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            geometry: {
+                type: "Point",
+                coordinates: [cities[random].lng, cities[random].lat]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/djk8lztrc/image/upload/v1618898954/CampGrounds/vxgokwq03wweqp12cndt.jpg',
